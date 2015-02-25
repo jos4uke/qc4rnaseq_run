61e981888773e8498f1f7df9fb60ecb1b38f1992
Joseph Tran <Joseph.Tran@versailles.inra.fr>
2015-02-25 23:25:19 +0100

    fix generic output path

--- RNASeq_QC_run.R
+++ RNASeq_QC_run.R
@@ -308,6 +308,7 @@ if ((!is.null(opt$stats)) && (!is.null(opt$design))) {
 ## Execute the .Rmd (prepare the plots in a .pdf file)
 ##
 
+outdir_abs_path <- normalizePath(opt$outdir)
 
 # Gather all checks and launch the appropriate.Rmd script
 if (all(is_stats_format, is_design_format, is_count_design, is_stats_design)) {
@@ -315,12 +316,12 @@ if (all(is_stats_format, is_design_format, is_count_design, is_stats_design)) {
 	if (is_bbric_format){
 		bbric_report_path <- system.file("report", "QC_RNASeq_Count_BBRIC.Rmd", package="qc4rnaseq")
 		bbric_report_file <- paste(unlist(strsplit(basename(bbric_report_path),".Rmd")), ".pdf", sep="")
-		outdir_abs_path <- normalizePath(opt$outdir)
 		render(input=bbric_report_path, output_format="pdf_document", output_file=bbric_report_file, output_dir=outdir_abs_path, intermediates_dir=outdir_abs_path)
 }	 else if (is_generic_format) {
 		generic_report_path <- system.file("report", "QC_RNASeq_Count_generic.Rmd", package="qc4rnaseq")
-		render(input=generic_report_path, output_format="pdf_document", output_dir=opt$outdir)
-	}
+		generic_report_file <- paste(unlist(strsplit(basename(generic_report_path),".Rmd")), ".pdf", sep="")
+		render(input=generic_report_path, output_format="pdf_document", output_file=generic_report_file, output_dir=outdir_abs_path, intermediates_dir=outdir_abs_path)	
+}
 	if (!is.null(opt$stats)) {
 		stats_report_path <- system.file("report", "QC_RNASeq_Stats_BBRIC.Rmd", package="qc4rnaseq")
 		render(input=stats_report_path, output_format="pdf_document", output_dir=opt$outdir)
